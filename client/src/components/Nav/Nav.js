feb33068357ea6263c18b8f3366e6dee60b531ae
chamaine <chamaine@wellington-studio.com>
2017-12-20 11:59:23 -0800

    new frontend tree

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { slide as Menu } from 'react-burger-menu'
 import "./Nav.css";
 
 class Navigation extends React.Component {
-
   showSettings (event) {
-    event.preventDefault();   
+    event.preventDefault();
+   
   }
 
-  render () {
+	render () {
     return (
-      <div>
-      </div>
+      <Menu>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="#">Donate</a>
+        <a id="contact" className="menu-item" href="#">Volunteer</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
     );
   }
 }
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Header from '../../components/Header';
+import Navigation from '../../components/Nav';
+
 import Content from '../../components/Content';
 
 // require("../assets/index.css")
