5428323cf0d2e6c3a3fd4a0b0f0d9f30a8973d73
chamaine <chamaine@wellington-studio.com>
2017-12-20 12:36:23 -0800

    login

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.css";
+
+class Footer extends React.Component {
+   render() {
+      	return (
+        	<div>
+        		<p>Copyright 2017 Naga and Chamaine</p>
+         	</div>
+      	);
+   	}
+}
+
+export default Footer;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -12,8 +12,8 @@ class Navigation extends React.Component {
     return (
       <Menu>
         <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="#">Donate</a>
-        <a id="contact" className="menu-item" href="#">Volunteer</a>
+        <a id="donations" className="menu-item" href="#">Donations</a>
+        <a id="users" className="menu-item" href="#">Users</a>
         <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
       </Menu>
     );
