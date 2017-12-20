c1ec2861969230ec1a42ffc7048cb3159bf2b8ec
nchandika1 <nchandika@gmail.com>
2017-12-20 13:44:22 -0800

    add googe auth

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Navigation from '../../components/Nav';
-
 import Content from '../../components/Content';
+import Login from '../../components/Login';
 
 // require("../assets/index.css")
 class Home extends React.Component {
    render() {
       return (
          <div>
-            <Content buttface={true} />            
+            <Content buttface={true} /> 
+            <Login />           
          </div>
       );
    }
