6efd4581211615b75956d52d0deab610178c22c4
chamaine <chamaine@wellington-studio.com>
2017-12-25 12:29:46 -0800

    updates

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,11 +4,11 @@ import "./Footer.css";
 class Footer extends React.Component {
    render() {
       	return (
-      		<div id="Container-fluid">
-        	<div className="footer-bottom">
+      	<div id="container-fluid">
+          <div className="footer-bottom">
         		<p>Copyright &#169;2017 Nagarani Chandika and Chamaine Woffard</p>
          	</div>
-         	</div>
+        </div>
       	);
    	}
 }
