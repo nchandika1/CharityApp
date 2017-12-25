687288d53c27804113b3df6e630b106090696a20
nchandika1 <nchandika@gmail.com>
2017-12-25 11:16:55 -0800

    fixed footer to stick to the bottom

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
+// Dependencies
 import React from 'react';
 import "./Footer.css";
 
+// Render footer for all pages
 class Footer extends React.Component {
-   render() {
-      	return (
-        	<div className="footer-bottom">
-        		<p>Copyright &#169;2017 Nagarani Chandika and Chamaine Woffard</p>
-         	</div>
-      	);
-   	}
+  render() {
+  	return (
+    	<div className="footer">
+    		<p>Copyright &#169;2017 Nagarani Chandika and Chamaine Woffard</p>
+     	</div>
+  	);
+ 	}
 }
 
 export default Footer;
