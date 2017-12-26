55cbd902f9eb406a97671c9b1d3223216fc30fcb
nchandika1 <nchandika@gmail.com>
2017-12-26 14:40:54 -0800

    added Navigation to Profile and state

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
+import Navigation from "../../components/Nav";
+import API from "../../utils/API";
 import "./Profile.css";
 
 class Profile extends Component {
 
+	state={
+		user: {}
+	}
+
+	componentDidMount() {
+		API.getUser(this.props.match.params.userid)
+      .then(res => {
+      		console.log(this.state);
+          console.log(res.data);
+          this.setState({user: res.data});
+        }
+      )
+	}
+
   render() {
-    return (
-      <div>
-        <p>"No Profile yet!"</p>
-      </div>    
-    );
+  	if (this.state.user) {
+  		return (
+  			<div>
+          <Navigation user={this.state.user.id} />
+	  			<p>Hello, {this.state.user.firstName} {this.state.user.lastName}!</p>
+	  			<hr />
+	  			
+  			</div>
+  		);
+  	} else {
+  		return(<p>No Profile information available for this user!</p>);
+  	}
   }
 }
 
