ff3e53fa0c30eceffe288e9144f0f0bbef7c5d88
chamaine <chamaine@wellington-studio.com>
2017-12-29 11:21:03 -0800

    volunteer page

--- client/src/pages/Volunteer/Volunteer.js
+++ client/src/pages/Volunteer/Volunteer.js
@@ -4,12 +4,22 @@ import API from "../../utils/API";
 import Navigation from '../../components/Nav';
 
 class Volunteer extends Component {
+  state = {
+    user: {}, // Represents user information
+    yearToDate: 0
+  };
+
+	componentDidMount() {
+    let elem = document.querySelector('body');
+    elem.classList.remove('large');
+    elem.classList.add('large2');
+  }
 
   render() {
     return (
       <div>
       	<Navigation />
-        <p>"No Volunteers yet!"</p>
+      	<div className="account-welcome">VOLUNTEER HOURS COMING SOON!</div>
       </div>    
     );
   }
