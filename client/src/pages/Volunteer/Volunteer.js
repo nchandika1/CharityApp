bbbdb5cdfb40cbb6da7116d8cae29babdead9d1e
chamaine <chamaine@wellington-studio.com>
2017-12-28 20:06:27 -0800

    volunteer

--- client/src/pages/Volunteer/Volunteer.js
+++ client/src/pages/Volunteer/Volunteer.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./Volunteer.css";
+import API from "../../utils/API";
+import Navigation from '../../components/Nav';
 
 class Volunteer extends Component {
 
   render() {
     return (
       <div>
+      	<Navigation />
         <p>"No Volunteers yet!"</p>
       </div>    
     );
