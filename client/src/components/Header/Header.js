c7475dba993c40021a03aacf25e33111b8088da5
nchandika1 <nchandika@gmail.com>
2017-12-29 15:14:44 -0800

    removed extra styling and also a semicolon showing up in the header logo

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,13 +2,11 @@ import React from 'react';
 import "./Header.css";
 import logo from './hopeapplogo2.png';
 
-console.log(logo); 
-
 function Header() {
 	return (
 		<div className=".header-style">
-	 <img src={logo} alt="Logo" />; 
-	</div>
+		 	<img src={logo} alt="Logo" />
+		</div>
 	);
 }
 
