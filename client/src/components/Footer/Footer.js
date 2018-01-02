5c48b2c02ad0c6c8c18f67729c43f35c412c8cef
chamaine <chamaine@wellington-studio.com>
2018-01-02 09:43:37 -0800

    socialshares

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,21 +1,22 @@
-// Dependencies
 import React from 'react';
 import "./Footer.css";
+import facebook from "../../assets/images/facebook.png";
 
 // Render footer for all pages
 class Footer extends React.Component {
-  render() {
-  	return (
-    	<div className="footer">
-    		<p className="left-text">Copyright ©2017 Nagarani Chandika and Chamaine Woffard.  All rights reserved.</p>
-    		<p className="right-text"> 
-    			<button> Privacy </button>
-    			<button> Terms </button>
-    			<button> Contact Us </button>
-    		</p>
-     	</div>
-  	);
- 	}
+ render() {
+     return (
+       <div className="footer">
+           <p className="left-text">Copyright ©2017 Nagarani Chandika and Chamaine Woffard.  All rights reserved.</p>
+           <p className="right-text">
+               <button> Privacy </button>
+               <button> Terms </button>
+               <button> Contact Us </button>
+         <a href="#"><img className="logoStyle" src={facebook} /></a>
+           </p>
+        </div>
+     );
+    }
 }
 
 export default Footer;
