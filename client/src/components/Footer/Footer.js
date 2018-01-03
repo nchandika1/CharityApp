87ade3855a1e054bc6089c3d12b1d7c4cff19ea9
chamaine <chamaine@wellington-studio.com>
2018-01-03 11:54:34 -0800

    Added social icons

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,21 +1,30 @@
 // Dependencies
 import React from 'react';
 import "./Footer.css";
+import facebook from "../../assets/images/facebook.png";
+import twitter from "../../assets/images/twitter.png";
+import linkedin from "../../assets/images/linkedin.png";
 
 // Render footer for all pages
 class Footer extends React.Component {
-  render() {
-  	return (
-    	<div className="footer">
-    		<p className="left-text">Copyright ©2017 Nagarani Chandika and Chamaine Woffard.  All rights reserved.</p>
-    		<p className="right-text"> 
-    			<button> Privacy </button>
-    			<button> Terms </button>
-    			<button> Contact Us </button>
-    		</p>
-     	</div>
-  	);
- 	}
+ render() {
+     return (
+       <div className="footer">
+           <p className="left-text">Copyright ©2017 Nagarani Chandika and Chamaine Woffard.  All rights reserved.</p>
+           <p className="right-text">
+              <a href="#"><img className="logoStyle" src={facebook} /></a>
+              <a href="#"><img className="logoStyle" src={twitter} /></a>
+              <a href="#"><img className="logoStyle" src={linkedin} /></a>
+              <br />
+              <button className="button"> Privacy </button>
+              <button className="button"> Terms </button>
+              <button className="button"> Contact Us </button>
+
+
+           </p>
+        </div>
+     );
+    }
 }
 
 export default Footer;
